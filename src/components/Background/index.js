2a92ea0a6c190df842d9be484bddefca80d73c7d
Gabriel Batista <svbgabriel@gmail.com>
2019-08-25 22:08:27 -0300

    Configura o Background

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinerGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinerGradient).attrs({
+  colors: ['#7159c1', '#ab59c1'],
+})`
+  flex: 1;
+`;

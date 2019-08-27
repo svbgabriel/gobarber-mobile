06b89a72711a52c4372186eb258615fe987d5225
Gabriel Batista <svbgabriel@gmail.com>
2019-08-26 22:27:47 -0300

    Configura a navegação por abas

--- src/routes.js
+++ src/routes.js
@@ -7,15 +7,29 @@ import {
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({ SignIn, SignUp }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#ffffff',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
